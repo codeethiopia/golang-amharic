b8632c8a170f3d69e8f239c7e1c3bccd97e45662
codeethiopia <ethiolinkd@gmail.com>
2018-07-16 19:17:16 -0400

    numbers

---
+++ 015_NumericTypes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// var a int
+// var b float64
+// var c rune
+
+func main(){
+x := 'ቀ'
+a := 4
+fmt.Printf("value: %v type: %T\n",x,x)
+fmt.Printf("value: %v type: %T\n",a,a)
+}
