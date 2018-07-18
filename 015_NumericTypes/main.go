e168dbf707602307adce0114a02c20370d5bf948
codeethiopia <ethiolinkd@gmail.com>
2018-07-18 09:16:20 -0400

    update

--- 015_NumericTypes/main.go
+++ 015_NumericTypes/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 )
 
-// var a int
-// var b float64
-// var c rune
-
+var a int
+var b int8
 func main(){
 x := 'ቀ'
-a := 4
-fmt.Printf("value: %v type: %T\n",x,x)
-fmt.Printf("value: %v type: %T\n",a,a)
+a = 5
+b = -10
+c := 42.4543646
+fmt.Printf("value:%v type:%T\n",a,a)
+fmt.Printf("value:%v type:%T\n",b,b)
+fmt.Printf("value:%v type:%T\n",c,c)
+fmt.Printf("value:%v type:%T\n",x,x)
 }
