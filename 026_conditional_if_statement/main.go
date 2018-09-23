e2997437b6fdb733435a026b600157cb9cfeded1
codeethiopia <ethiolinkd@gmail.com>
2018-09-22 22:22:24 -0400

    if statement

---
+++ 026_conditional_if_statement/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	if true {
+		fmt.Println("001")
+	}
+
+	if false {
+		fmt.Println("002")
+	}
+
+	if 2 == 2 {
+		fmt.Println("003")
+	}
+
+	if 2 != 3 {
+		fmt.Println("004")
+	}
+}
