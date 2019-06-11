8988bc2ac49080902f0037a03db09db05f65a20f
codeethiopia <ethiolinkd@gmail.com>
2019-06-11 09:00:07 -0400

    anonymous func

--- 049_anonymous_function/main.go
+++ 049_anonymous_function/main.go
@@ -5,5 +5,10 @@ import (
 )
 
 func main() {
-	func() { fmt.Println("anonymous function") }()
+	func() { fmt.Println("anonymous function executed") }()
+
+	func(x int, y int) {
+		fmt.Printf("%d + %d = %d\n", x, y, x+y)
+	}(2, 3)
+
 }
